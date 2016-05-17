60daab316da4dfe54cea008d0d74f929485009ef
Luan Pontes <luanpontes2@gmail.com>
2016-05-17 19:50:37 -0300

    init

---
+++ app.rb
@@ -0,0 +1,44 @@
+
+
+require 'sinatra'
+require 'strans-client'
+
+get '/linhas.json' do
+  client = StransClient.new('luanpontes2@gmail.com', 'naul1991', '49ea6f5525a34e71bdd7b4f8a92adaac')
+  client.autentic
+  client.linhas(params[:busca]).to_s
+end
+
+get '/paradas.json' do
+  client = StransClient.new('luanpontes2@gmail.com', 'naul1991', '49ea6f5525a34e71bdd7b4f8a92adaac')
+  client.autentic
+  client.paradas(params[:busca]).to_s
+end
+
+get '/paradasLinha.json' do
+  if !params[:busca]
+    status 403
+    body "Informe Parametro busca na url"
+  else
+    client = StransClient.new('luanpontes2@gmail.com', 'naul1991', '49ea6f5525a34e71bdd7b4f8a92adaac')
+    client.autentic
+    client.paradas_linha(params[:busca]).to_s
+  end
+end
+
+get '/veiculos.json' do
+  client = StransClient.new('luanpontes2@gmail.com', 'naul1991', '49ea6f5525a34e71bdd7b4f8a92adaac')
+  client.autentic
+  client.veiculos().to_s
+end
+
+get '/veiculosLinha.json' do
+  if !params[:busca]
+    status 403
+    body "Informe Parametro busca na url"
+  else
+    client = StransClient.new('luanpontes2@gmail.com', 'naul1991', '49ea6f5525a34e71bdd7b4f8a92adaac')
+    client.autentic
+    client.veiculos_linha(params[:busca]).to_s
+  end
+end
